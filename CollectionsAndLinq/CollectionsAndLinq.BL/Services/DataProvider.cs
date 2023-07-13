00abe719063a4d43f4d42f2cb69a6abca8e4d520
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-07-13 21:42:53 +0300

    Added DataProvider class to the project

---
+++ CollectionsAndLinq/CollectionsAndLinq.BL/Services/DataProvider.cs
@@ -0,0 +1,28 @@
+﻿using CollectionsAndLinq.BL.Entities;
+using CollectionsAndLinq.BL.Interfaces;
+
+namespace CollectionsAndLinq.BL.Services
+{
+    public class DataProvider : IDataProvider
+    {
+        public Task<List<Project>> GetProjectsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Entities.Task>> GetTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Team>> GetTeamsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
